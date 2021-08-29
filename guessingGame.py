19f73fc00ff07bbf95161d71f919319dee4150ef
ranvirdhanak <72129904+ranvirdhanak@users.noreply.github.com>
2021-08-29 14:01:59 +0530

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -1,25 +1,28 @@
-number = input("guess the number: ")
-
-print(number)
-
-number = 0
-
-for i in number:
-    number = number + 1
+import random
+rand = random.randint(1,9)
 
 
-print(number)
+
+
+chances = 0
+
+
 
 # While loop to count the number of chances
 
 while chances < 5:
-
-if guess == number:
+    number = int(input("guess the number: "))
+    if rand == number:
 # if number entered by user is same as the generated
 # number by randint function then break from loop using loop
 # control statement "break"
-print("CONGRATULATIONS YOU WON!!!")
-break
+        print("CONGRATULATIONS YOU WON!!!")
+        break
+    elif number < rand:
+        print("Guess a higher number")
+    else:
+        print("Guess a lower number")
+    chances += 1
 
 if not chances < 5:
-print("YOU LOSE!!! The number is", number)
+    print("YOU LOSE!!! The number is", number)
